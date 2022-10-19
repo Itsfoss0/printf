bcd6e6a82752ecb4fa3c085a073594bdf4d0d425
Ebenezer <akhonyaebenezer@gmail.com>
2022-10-19 16:00:49 +0300

    Added int print_bigS(va_list l, flags_t *f) function prototype in the main.h header file

--- main.h
+++ main.h
@@ -14,6 +14,7 @@ int get_flag(char s, flags_t *f);
 int (*get_print(char s))(va_list, flags_t *);
 int print_rot13(va_list l, flags_t *f);
 int print_rev(va_list l, flags_t *f);
+int print_bigS(va_list l, flags_t *f);
 
 /**structures **/
 /**
