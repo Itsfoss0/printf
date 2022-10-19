820bff6965660e118257681122847fc01a51e454
Ebenezer <akhonyaebenezer@gmail.com>
2022-10-19 15:57:35 +0300

    Added int print_rot13(va_list l, flags_t *f) function prototype in the main.h header file

--- main.h
+++ main.h
@@ -12,6 +12,8 @@ int print_char(va_list l, flags_t *f);
 int print_string(va_list l, flags_t *f);
 int get_flag(char s, flags_t *f);
 int (*get_print(char s))(va_list, flags_t *);
+int print_rot13(va_list l, flags_t *f);
+
 
 /**structures **/
 /**
