72f28af055961dab06b536931e71af474f32ec0c
Ebenezer <akhonyaebenezer@gmail.com>
2022-10-19 15:16:53 +0300

    Added the flags structure in the main.h header file

--- main.h
+++ main.h
@@ -1,9 +1,27 @@
 #ifndef MAIN_H
 #define MAIN_H
-
+/** include stdlibs **/
 #include <stdlib.h>
 #include <stdarg.h>
 
 /** function prototypes**/
 int _printf(const char *format, ...);
 int _putchar(char c);
+
+
+/**structures **/
+/**
+ * struct flags - struct containing flags to "turn on"
+ * when a flag specifier is passed to _printf()
+ * @plus: flag for the '+' character
+ * @space: flag for the ' ' character
+ * @hash: flag for the '#' character
+ */
+typedef struct flags
+{
+    int plus;
+    int space;
+    int hash;
+} flags_t;
+
+#endif
